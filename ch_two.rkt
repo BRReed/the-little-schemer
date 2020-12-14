67b221562124443520d93ea81d59bbc5364bbb61
Brian R Reed <b.r.reed@gmail.com>
2020-12-14 15:29:58 -0800

    chapter 2 in progress

--- ch_two.rkt
+++ ch_two.rkt
@@ -3,3 +3,20 @@
 ;    4th Ed.
 (define (atom? x)
   (and (not (pair? x)) (not (null? x))))
+
+(define (lat? l)
+  (cond
+    ((null? l) #t)
+    ((atom? (car l)) (lat? (cdr l)))
+    (else #f)))
+  
+
+(lat? '(Jack Sprat could eat no chicken fat))
+; true bcause each s-exp in list is an atom
+(lat? '((Jack) Sprat could eat no chicken fat))
+; false because (jack) is a list
+(lat? '(Jack (Sprat could) eat no chicken fat))
+; false because cdr of list has a list in it
+(lat? '())
+; true because it is an empty list
+; True or False a lat is a list of atoms (true, but also an empty list)
